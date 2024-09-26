6bbf6435fce19ccfe45c69bfa2866d8cb05bbdaa
jeric.aminola <jeric.aminola@msugensan.edu.ph>
2024-09-26 13:53:00 +0000

    Basic Crud

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,18 @@
+# from django.urls import path
+# from .views import blog_list, blog_detail, blog_delete
+
+# urlpatterns = [
+#     path('', blog_list),
+#     path('<id>/', blog_detail),
+#     path('<id>/delete/', blog_delete),
+# ]
+
 from django.urls import path
-from .views import blog_list, blog_detail, blog_delete
+from .views import blog_list, blog_create, blog_update, blog_delete
 
 urlpatterns = [
-    path('', blog_list),
-    path('<id>/', blog_detail),
-    path('<id>/delete/', blog_delete),
+    path('', blog_list, name='blog_list'),
+    path('post/new/', blog_create, name='blog_create'),
+    path('post/<int:pk>/edit/', blog_update, name='blog_update'),
+    path('post/<int:pk>/delete/', blog_delete, name='blog_delete'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .models import Post
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect
+# from .models import Post
 
-# Create your views here.
+# # Create your views here.
+
+# def blog_list(request):
+#     post = Post.objects.all()
+#     context = {
+#         'blog_list': post
+#     }
+#     return render(request, 'blog/blog_list.html', context)
+
+# def blog_detail(request, id):
+#     each_post = Post.objects.get(id=id)
+#     context = {
+#         'blog_detail': each_post
+#     }
+#     return render(request, 'blog/blog_detail.html', context)
+
+# def blog_delete(request, id):
+#     each_post = Post.objects.get(id=id)
+#     each_post.delete()
+#     return HttpResponseRedirect('/posts/')
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
 def blog_list(request):
-    post = Post.objects.all()
-    context = {
-        'blog_list': post
-    }
-    return render(request, 'blog/blog_list.html', context)
+    posts = Post.objects.all()
+    return render(request, 'blog/blog_list.html', {'posts': posts})
 
-def blog_detail(request, id):
-    each_post = Post.objects.get(id=id)
-    context = {
-        'blog_detail': each_post
-    }
-    return render(request, 'blog/blog_detail.html', context)
+def blog_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/blog_form.html', {'form': form})
 
-def blog_delete(request, id):
-    each_post = Post.objects.get(id=id)
-    each_post.delete()
-    return HttpResponseRedirect('/posts/')
+def blog_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/blog_form.html', {'form': form})
+
+def blog_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog_list')
+    return render(request, 'blog/blog_confirm_delete.html', {'post': post})
